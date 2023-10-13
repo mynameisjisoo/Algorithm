883c07ed9f1fcf86a111821532544bd926ee11bb
Jisoo Lee <84840032+mynameisjisoo@users.noreply.github.com>
2023-10-13 12:30:19 +0900

    [level unrated] Title: 수열과 구간 쿼리 3, Time: 0.38 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.js
+++ 프로그래머스/unrated/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.js
@@ -1,9 +1,6 @@
 function solution(arr, queries) {
-   queries.forEach((query)=> {
-       const i = query[0]; const j =query[1];
-       const temp = arr[i];
-       arr[i]= arr[j];
-       arr[j]= temp;
+   queries.forEach(([i,j])=> {
+        [arr[i],arr[j]] = [arr[j],arr[i]]
    })
     return arr;
 }
