050c18cce410ad87ac69d3284735c804537b3d7e
Jisoo Lee <84840032+mynameisjisoo@users.noreply.github.com>
2023-11-10 16:08:10 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -1,28 +1,4 @@
 function solution(n, arr1, arr2) {
-    
-    const decimalToBinary =(array)=> array.map(num=> {
-        const binaryArr =[];
-        let decimal =num;
-        for(let i=n; i>0; i--) {
-             const pow = Math.pow(2,i-1);
-             if(pow > decimal) {
-                binaryArr.push(0);
-             }
-             if(pow === decimal){
-                decimal-=pow;
-                 binaryArr.push(1);
-             }
-             if(pow < decimal) {
-                decimal-=pow;
-                 binaryArr.push(1);
-             }
-            }
-        return binaryArr
-    })
-    
-    const answer= decimalToBinary(arr1).map((arr,i)=>{
-        return arr.map((el, j)=> el===1 || decimalToBinary(arr2)[i][j]===1 ? "#" : " ").join("")
-    })
-    return answer
+    return arr1.map((el,i)=> (el|arr2[i]).toString(2).padStart(n,"0")).map(el=>el.replace(/1/g,"#").replace(/0/g," "))
 
 }
